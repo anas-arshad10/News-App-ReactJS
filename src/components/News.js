3880ea4266f1f99d373736a812a336285febacfa
Anas <anas.arshad10@gmail.com>
2023-04-20 23:55:56 +0500

    First Commit

---
+++ src/components/News.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import NewsItem from './NewsItem'
+
+export class News extends Component {
+  render() {
+    return (
+      <div className="container my-3">
+        <h2>News App - Top Headlines</h2>
+
+        <div className="row">
+            <div className="col-md-3">
+                <NewsItem title="My Title" description="my Descp"/>
+            </div>
+            <div className="col-md-3">
+                <NewsItem title="My Title" description="my Descp"/>
+            </div>
+            <div className="col-md-3">
+                <NewsItem title="My Title" description="my Descp"/>
+            </div>
+
+        </div>
+      </div>
+    )
+  }
+}
+
+export default News
