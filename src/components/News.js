8e436529d33be121d082192c2302179bd39810b3
Anas <anas.arshad10@gmail.com>
2023-05-02 23:36:35 +0500

    Updated News App

--- src/components/News.js
+++ src/components/News.js
@@ -2,21 +2,34 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
+constructor(){
+  super();
+  console.log("this is")
+  this.state={
+    articles: this.articles,
+    loading: false
+  }
+}
+async componentDidMount(){
+  let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=cbd1dc15e619454f8fee23954d3dae3c";
+  let data = await fetch(url);
+  let  parsedData = await data.json();
+  this.setState({articles: parsedData.articles})
+}
+
   render() {
     return (
       <div className="container my-3">
         <h2>News App - Top Headlines</h2>
 
         <div className="row">
-            <div className="col-md-3">
-                <NewsItem title="My Title" description="my Descp"/>
-            </div>
-            <div className="col-md-3">
-                <NewsItem title="My Title" description="my Descp"/>
-            </div>
-            <div className="col-md-3">
-                <NewsItem title="My Title" description="my Descp"/>
-            </div>
+          {this.state.articles.map((element)=>{
+            return <div className="col-md-3" key={element.url}>
+            <NewsItem  title={element.title} description={element.description} imageUrl={element.urlToImage} newsUrl={element.url}/>
+        </div>
+
+          })}
+            
 
         </div>
       </div>
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,16 +2,16 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
     render() {
-        let {title, description} = this.props;
+        let { title, description, imageUrl, newsUrl } = this.props;
         return (
-            <div >
-                
-                <div className="card" style={{width: "18rem"}}>
-                    <img src="..." className="card-img-top" alt="..." />
+            <div className="my-3">
+
+                <div className="card" style={{ width: "18rem" }}>
+                    <img src={!imageUrl?"https://cdn.vox-cdn.com/thumbor/hQgwvxiu89tZr73687st0NUap9E=/0x0:2040x1360/1200x628/filters:focal(1020x680:1021x681)/cdn.vox-cdn.com/uploads/chorus_asset/file/21869421/akrales_200904_4160_0135.0.jpg":imageUrl} className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{title}</h5>
                         <p className="card-text">{description}</p>
-                        <a href="/newsdetails" className="btn btn-primary">Go somewhere</a>
+                        <a href={newsUrl} target="_blank" className="btn btn-primary">Go somewhere</a>
                     </div>
                 </div>
             </div>
