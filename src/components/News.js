e7a430e851fc010740038004a5867e3734d28759
Anas <anas.arshad10@gmail.com>
2023-05-10 22:48:43 +0500

    Updated News App

--- src/components/News.js
+++ src/components/News.js
@@ -6,15 +6,42 @@ constructor(){
   super();
   console.log("this is")
   this.state={
-    articles: this.articles,
-    loading: false
+    articles: [],
+    loading: false,
+    page: 1
   }
 }
 async componentDidMount(){
-  let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=cbd1dc15e619454f8fee23954d3dae3c";
+  let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=cbd1dc15e619454f8fee23954d3dae3c&page=1pageSize=20";
   let data = await fetch(url);
   let  parsedData = await data.json();
-  this.setState({articles: parsedData.articles})
+  this.setState({articles: parsedData.articles, totalResults: parsedData.totalResults})
+}
+handlePrevClick= async ()=>{
+console.log("Previious");
+let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=cbd1dc15e619454f8fee23954d3dae3c&page=${this.state.page - 1}&pageSize=20`;
+  let data = await fetch(url);
+  let  parsedData = await data.json();
+this.setState({
+  page: this.state.page - 1,
+  articles: parsedData.articles
+})
+}
+
+handleNextClick = async ()=>{
+console.log("Next");
+if (this.state.page + 1 > Math.ceil(this.state.totalResults/20)){
+
+}
+else {
+  let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=cbd1dc15e619454f8fee23954d3dae3c&page=${this.state.page + 1}&pageSize=20`;
+  let data = await fetch(url);
+  let  parsedData = await data.json();
+this.setState({
+  page: this.state.page + 1,
+  articles: parsedData.articles
+})
+}
 }
 
   render() {
@@ -31,6 +58,10 @@ async componentDidMount(){
           })}
             
 
+        </div>
+        <div className="container d-flex justify-content-between">
+        <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}>Previous</button>
+        <button disabled={this.state.page>=this.state.totalResults/20} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next</button>
         </div>
       </div>
     )
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -11,7 +11,7 @@ export class NewsItem extends Component {
                     <div className="card-body">
                         <h5 className="card-title">{title}</h5>
                         <p className="card-text">{description}</p>
-                        <a href={newsUrl} target="_blank" className="btn btn-primary">Go somewhere</a>
+                        <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-dark">Go somewhere</a>
                     </div>
                 </div>
             </div>
