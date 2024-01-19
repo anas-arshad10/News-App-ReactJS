2abc6351fac60f3a7c0743e02692591e210aa861
anas-arshad10 <anas.arshad10@gmail.com>
2024-01-19 23:17:05 +0500

    Added build folder

--- src/components/News.js
+++ src/components/News.js
@@ -10,81 +10,72 @@ const News = (props) => {
   const [page, setPage] = useState(1);
   const [totalResults, setTotalResults] = useState(0);
 
-  document.title = `${props.category} - NewsApp`;
 
-  /* handlePrevClick = async () => {
+    /* handlePrevClick = async () => {
 
-  setPage( page - 1 );
-  updateNews();
-}
+    setPage( page - 1 );
+    updateNews();
+  }
 
-handleNextClick = async () => {
-  setPage( page + 1 );
-  updateNews();
-} */
+  handleNextClick = async () => {
+    setPage( page + 1 );
+    updateNews();
+  } */
 
-  const updateNews = async (currentPage) => {
+  const updateNews = async () => {
     props.setProgress(10);
-    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=cbd1dc15e619454f8fee23954d3dae3c&page=${currentPage}&pageSize=${props.pageSize}`;
-    setLoading(true);
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=cbd1dc15e619454f8fee23954d3dae3c&page=${page}&pageSize=${props.pageSize}`;
+    setLoading(false);
     let data = await fetch(url);
     props.setProgress(30);
     let parsedData = await data.json();
     props.setProgress(70);
-    setArticles(articles.concat(parsedData.articles));
+    setArticles(parsedData.articles);
     setTotalResults(parsedData.totalResults);
     setLoading(false);
     props.setProgress(100);
   };
 
   useEffect(() => {
-    updateNews(page);
-  }, [page]);
+    document.title = `${props.category} - NewsApp`;
+
+    updateNews();
+  }, []);
+  
 
   const fetchMoreData = async () => {
-    const nextPage = page + 1;
-    setPage(nextPage);
-    updateNews(nextPage);
+    props.setProgress(10);
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=cbd1dc15e619454f8fee23954d3dae3c&page=${page+1}&pageSize=${props.pageSize}`;
+    setPage( page + 1 );
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    setArticles(articles.concat(parsedData.articles));
+    setTotalResults(parsedData.totalResults);
   };
 
   return (
     <>
-      <h1 className='text-center'>News App - Top {props.category} Headlines</h1>
+      <h1 className='text-center' style={{marginTop:'90px'}}>News App - Top {props.category} Headlines</h1>
       {loading && <Spinner />}
       <InfiniteScroll
         dataLength={articles.length}
         next={fetchMoreData}
         hasMore={articles.length !== totalResults}
-        loader={<h4><Spinner /></h4>}
+        loader={<h4><Spinner/></h4>}
       >
         <div className="container">
-          <div className="text-center row">
-            {articles.map((element, index) => (
-              <div className="col-md-4" key={`${element.url}-${index}`}>
-                <NewsItem
-                  title={element.title}
-                  description={element.description}
-                  imageUrl={element.urlToImage}
-                  newsUrl={element.url}
-                  author={element.author}
-                  date={element.publishedAt}
-                  source={element.source.name}
-                />
-              </div>
-            ))}
-          </div>
-          {/*       <div className="text-center row">
+             <div className="text-center row">
           {articles.map((element) => {
             return <div className="col-md-4" key={element.url}>
               <NewsItem title={element.title} description={element.description} imageUrl={element.urlToImage} 
               newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
         </div>
           })}
-        </div> */}
-
+        </div>
+          
         </div>
       </InfiniteScroll>
-      {/*    <div className="container d-flex justify-content-between">
+       {/*    <div className="container d-flex justify-content-between">
           <button disabled={page <= 1} type="button" className="btn btn-dark" onClick={handlePrevClick}>
             Previous
           </button>
